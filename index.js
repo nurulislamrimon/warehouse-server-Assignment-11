52e16cf7a050c3989c3c60a250abef37d8de4a22
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-06-06 16:51:07 +0600

    home done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,22 +22,52 @@ function run() {
         // banner collection
         const bannerCollection = client.db('bannerCollection').collection('banner')
 
-        // get all inventories
-        app.get('/inventories', async (req, res) => {
-            const query = {};
-            const cursor = inventoryCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result)
-            console.log('inventories responding');
-        })
-
-        // get all banners
+        // read all banners
         app.get('/banners', async (req, res) => {
             const query = {};
             const cursor = bannerCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
             console.log('banners responding');
+        })
+        // read all inventory
+        app.get('/inventory', async (req, res) => {
+            const limit = parseInt(req.query.limit);
+            const sort = { sold: parseInt(req.query.sort) };
+            const query = {};
+            const cursor = inventoryCollection.find(query).sort(sort).limit(limit);
+            const result = await cursor.toArray();
+            res.send(result)
+            console.log(req.query, 'inventory responding');
+        })
+        // read the inventory selected for update 
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await inventoryCollection.findOne(query);
+            res.send(result);
+            console.log(id, 'id selected');
+        })
+        // update inventory
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const newDatas = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: newDatas.name,
+                    picture: newDatas.picture,
+                    price: newDatas.price,
+                    quantity: newDatas.quantity,
+                    description: newDatas.description,
+                    supplier: newDatas.supplier,
+                    sold: newDatas.sold
+                }
+            }
+            const result = await inventoryCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+            console.log(newDatas);
         })
 
 
